55abfd548d13e2bf0c26a0a1a44ba83162844998
Marcus Evans <evanmarc@gmail.com>
2017-07-03 15:30:19 -0700

    Moving multiple sentiment code out of Gutenberg Downloader

--- Gutenberg Downloader.R
+++ Gutenberg Downloader.R
@@ -156,31 +156,3 @@ oz %>%
   filter(!word %in% c("cowardly", "wicked", "witch", "magic")) %>%
   acast(word ~ sentiment, value.var = "n", fill = 0) %>%
   comparison.cloud(colors = c("red", "blue"), max.words = 50)
-
-
-
-
-afinn <- unnest_pan %>%
-  inner_join(get_sentiments("afinn")) %>%
-  group_by(index = linenumber %/% 80) %>%
-  summarise(sentiment = sum(score)) %>%
-  mutate(method = "AFINN")
-
-bing_and_nrc <- bind_rows(
-  unnest_pan %>%
-    inner_join(get_sentiments("bing")) %>%
-    mutate(method = "Bing et al."),
-  unnest_pan %>%
-    inner_join(get_sentiments("nrc")) %>%
-    filter(sentiment %in% c("positive", "negative")) %>%
-    mutate(method = "NRC")
-  ) %>%
-    count(method, index = linenumber %/% 80, sentiment) %>%
-    spread(sentiment, n, fill = 0) %>%
-    mutate(sentiment = positive - negative)
-
-bind_rows(afinn, bing_and_nrc) %>%
-  ggplot(aes(index, sentiment, fill = method)) +
-  geom_col(show.legend = F) +
-  facet_wrap(~method, ncol = 1, scales = "free_y") +
-  ggtitle("Peter Pan")
